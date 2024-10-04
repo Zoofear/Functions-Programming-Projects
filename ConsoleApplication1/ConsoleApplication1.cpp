f4ec853587753ac3ef9d6db6b4f2b9ef5c7d1f3d
Zoofear <misterz0312@gmail.com>
2024-10-04 18:21:49 -0400

    Finished program 2 by finishing the main

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
     //Prototype functions
 
-void inflationInput(double& x, double& y, double);                              //This function asks the user for input of the price of the item during the current year and the last two years
+void inflationInput(double& x, double& y, double& z);                              //This function asks the user for input of the price of the item during the current year and the last two years
 
 void inflationCalc(double x, double y, double z, double& calc1, double& calc2); //This function calculates the two inflations for this year and the year before that 
 
@@ -23,7 +23,14 @@ int main()
 
     double inflation1, inflation2;  //These variables are the inflation rates of this year and the year before
 
-    inflationInput(price0, price1, price2)
+    inflationInput(price0, price1, price2);
+
+    inflationCalc(price0, price1, price2, inflation1, inflation2);
+
+    inflationOutput(inflation1, inflation2);
+
+    return 0;
+
 }
 
 void inflationInput(double& x, double& y, double& z)
